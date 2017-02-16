37ef47419393230c0a26842fef6b0e48a4feb609
Быков Алексей <alexey-bycov@yandex.ru>
2017-02-16 11:01:27 +0700

    Add verifyInput() function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int verifyInput(int deposit, int days) {
+	if(days < 0) return 1;
+	if(days > 365) return 2;
+	if(deposit < 10000) return 3;
+	return 0;
+}
+
 int main() {
 	int deposit, days;
 	cout << "Введите размер вклада: ";
