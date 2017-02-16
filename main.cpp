9d078868bed1adf78b0cb69b89da6ddb65f93c45
Быков Алексей <alexey-bycov@yandex.ru>
2017-02-16 11:17:07 +0700

    Use verifyInput() in main() and use its result to print needed error message

--- main.cpp
+++ main.cpp
@@ -13,8 +13,21 @@ int main() {
 	int deposit, days;
 	cout << "Введите размер вклада (минимум 10000): ";
 	cin >> deposit;
-	cout << "Введите срок вклада (до 365 дней): ";
+	cout << "Введите срок вклада (не более 365 дней): ";
 	cin >> days;
+	
+	int verifResult = verifyInput(deposit, days);
+	switch(verifResult) {
+	case 1:
+		cout << "Срок не может быть отрицательным" << endl; 
+		return 1;
+	case 2:
+		cout << "Срок должен быть не более 365 дней" << endl;
+		return 2;
+	case 3:
+		cout << "Размер вклада не должен быть меньше 10000" << endl;
+		return 3;
+	}
 	
 
 	return 0;
