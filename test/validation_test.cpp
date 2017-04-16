cdd491008c6f9080afad28a5658c18ec20fd6446
Быков Алексей <alexey-bycov@yandex.ru>
2017-04-16 22:44:52 +0700

    Knowingly fail while testing

--- test/validation_test.cpp
+++ test/validation_test.cpp
@@ -6,7 +6,7 @@ CTEST(validationSuite, validationDays) {
 	ASSERT_EQUAL(1, verifyDays(-1));
 	
 	ASSERT_EQUAL(0, verifyDays(0));
-	ASSERT_EQUAL(0, verifyDays(100));
+	ASSERT_EQUAL(1150, verifyDays(100));
 	ASSERT_EQUAL(0, verifyDays(365));
 	
 	ASSERT_EQUAL(2, verifyDays(366));
